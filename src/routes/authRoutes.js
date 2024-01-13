f568360a48d856b15e077f1dea0dfacef0020592
Johnpaul <johnpaulokache@gmail.com>
2024-01-13 13:17:04 +0100

    refactor authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,7 +4,7 @@ const authController = require("../controllers/authController");
 
 // routes
 router.get("/login", authController.get_login);
-// router.post("/login", authController.post_login);
+router.post("/login", authController.post_login);
 router.get("/signup", authController.get_signup);
 router.post("/signup", authController.post_signup);
 
